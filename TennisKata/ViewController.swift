b015eb73472d3622dda7c68bd2775d44286e6864
Burak Koray Kose <burakkorayk@gmail.com>
2018-12-22 12:32:25 +0300

    Initial Commit

---
+++ TennisKata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TennisKata
+//
+//  Created by Burak Koray Kose on 22.12.2018.
+//  Copyright © 2018 Burak Koray Kose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
