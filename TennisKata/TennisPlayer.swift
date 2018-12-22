56f9d3b13ad53d680361ccdb4380df9c22aaee97
Burak Koray Kose <burakkorayk@gmail.com>
2018-12-22 13:29:51 +0300

    TennisPlayer created and score tested

---
+++ TennisKata/TennisPlayer.swift
@@ -0,0 +1,36 @@
+//
+//  TennisPlayer.swift
+//  TennisKata
+//
+//  Created by Burak Koray Kose on 22.12.2018.
+//  Copyright © 2018 Burak Koray Kose. All rights reserved.
+//
+
+import Foundation
+
+class TennisPlayer {
+    
+    enum Score :Int{
+        case Love
+        case Fifteen
+        case Thirty
+        case Forty
+        case Advantage
+        
+        mutating func next(){
+            self = Score(rawValue: rawValue + 1) ?? Score(rawValue: 0)!
+        }
+    }
+    
+    var score: Score = Score.Love
+    let name : String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func scored()  {
+        score.next()
+    }
+
+}
